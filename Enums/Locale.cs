09a30b0feb4cfa92161386a6ea70fb4d4bf34858
Artem Konkin <temakonkin@gmail.com>
2024-12-12 17:41:16 +0600

    - Refactor

---
+++ Enums/Locale.cs
@@ -0,0 +1,27 @@
+﻿namespace SoftResult.Enums;
+
+/// <summary>
+/// Перечисление локализации для указания языка сообщений результатов
+/// </summary>
+public enum Locale
+{
+    /// <summary>
+    /// Локализация не определена
+    /// </summary>
+    Undefined = 0,
+
+    /// <summary>
+    /// Русский язык
+    /// </summary>
+    Rus = 1,
+
+    /// <summary>
+    /// Кыргызский язык
+    /// </summary>
+    Kyr = 1,
+
+    /// <summary>
+    /// Английский язык
+    /// </summary>
+    Eng = 1
+}
